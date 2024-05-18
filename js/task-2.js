212f31de5f513890174e0a57e77b5850e2190960
DmytroPryadko <160414230+DmytroPryadko@users.noreply.github.com>
2024-05-18 16:18:50 +0200

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,38 @@
-function getShippingMessage(country, price, deliveryFee){
-    return `Shipping to ${country} will cost ${price + deliveryFee} credits`
-}
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
-
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+function calcAverageCalories(days) {
+    const howDays = days.length;
+    if (howDays === 0) return 0;
+    let totalCalories = 0;
+    for (const calories of days) {
+      totalCalories += calories.calories;
+    }
+    return totalCalories / howDays;
+  }
+  
+  console.group('TASK-2');
+  
+  console.log(
+    calcAverageCalories([
+      { day: 'monday', calories: 3010 },
+      { day: 'tuesday', calories: 3200 },
+      { day: 'wednesday', calories: 3120 },
+      { day: 'thursday', calories: 2900 },
+      { day: 'friday', calories: 3450 },
+      { day: 'saturday', calories: 3280 },
+      { day: 'sunday', calories: 3300 },
+    ])
+  ); // 3180
+  
+  console.log(
+    calcAverageCalories([
+      { day: 'monday', calories: 2040 },
+      { day: 'tuesday', calories: 2270 },
+      { day: 'wednesday', calories: 2420 },
+      { day: 'thursday', calories: 1900 },
+      { day: 'friday', calories: 2370 },
+      { day: 'saturday', calories: 2280 },
+      { day: 'sunday', calories: 2610 },
+    ])
+  ); // 2270
+  
+  console.log(calcAverageCalories([])); //00
+  console.groupEnd;
--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,24 @@
-function getElementWidth(content, padding, border) {
-    return Number.parseFloat(content)  + Number.parseFloat(padding) * 2  + Number.parseFloat(border) * 2 
-}
-
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+const profile = {
+    username: 'Jacob',
+    playTime: 300,
+    getInfo() {
+      return `${this.username} has ${this.playTime} active hours!`;
+    },
+    changeUsername(newName) {
+      this.username = newName;
+    },
+    updatePlayTime(hours) {
+      this.playTime += hours;
+    },
+  };
+  
+  console.group('TASK-3');
+  
+  console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+  
+  profile.changeUsername('Marco');
+  console.log(profile.getInfo()); // "Marco has 300 active hours!"
+  
+  profile.updatePlayTime(20);
+  console.log(profile.getInfo()); // "Marco has 320 active hours!"
+  console.groupEnd;
